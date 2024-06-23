dfd44ef11c2f00af1b2e4f668bd7fdbce1f960c0
Stormsurge01 <156475637+Stormsurge01@users.noreply.github.com>
2024-06-23 13:40:55 +0330

    Mine.tsx

--- src/components/Mine.tsx
+++ src/components/Mine.tsx
@@ -11,11 +11,11 @@ const Mine = () => {
     const [floatingTexts, setFloatingTexts] = useState<FloatingText[]>([]);
     const [nextId, setNextId] = useState(0);
     const [energy, setEnergy] = useState(maxEnergy);
-    const handleClick = (e:any) =>{
+    const handleClick = (e: any) =>{
         if (energy){
             setValue((perv) => perv +1);
             setEnergy((perv) => (perv > 0 ? perv - 1 : 0));
-            const {clinteX: x, clinteY: y} = e;
+            const {clientX: x, clientY: y} = e;
             const newText : FloatingText = {id: nextId,x,y};
             setFloatingTexts((perv)=>[...perv,newText]);
             setNextId ((perv)=>perv + 1); 
@@ -50,6 +50,24 @@ return (
     <div className="flex items-center justify-center">
         <img src={coin} alt="coin" onClick={handleClick} className="w-[70%] cursor-pointer drop-shadow-2xl coin-button"/>
     </div>
+    <div className="w-full flex flex-col gap-2">
+        <div className="flex w-full items-center justify-between">
+            <span className="text-[15px]">Energy ziad</span>
+            <span className="text-[15px] font-semibold">
+                {" "}
+                {energy} / {maxEnergy}
+            </span>
+        </div>
+        <div className="w-full relative rounded-full h-[16px] bg-[#012237] border border-[#073755]">
+            <div className="absolute left-0 h-full rounded-full bg-gradient-to-r from-[#dc7b0c] to-[#fff973]"
+            style={{width: `${energyPercentage}%`}}>
+            </div>
+        </div>
+    </div>
+    {floatingTexts.map((text)=>(
+        <span className="floating-text font-semibold text-[30px]"
+        key={text.id} style={{top:text.y, left:text.x}}>+1</span>
+    ))}
 </div>
 )
 }
